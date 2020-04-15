837679dae22c84c4d0416ccb766bd8803614e273
GX-Freedom <kjy911206@gmail.com>
2020-04-16 03:32:17 +0900

    Updates

---
+++ precache-manifest.32d7324c5a609672959e547856dad77f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39bebb801ad13ef9c357173829df174b",
+    "url": "/react_native_challenge/index.html"
+  },
+  {
+    "revision": "79a304182303d1fb25df",
+    "url": "/react_native_challenge/static/js/2.b8b6ed31.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_native_challenge/static/js/2.b8b6ed31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be9c166c0e08068ee43f",
+    "url": "/react_native_challenge/static/js/main.f8c2edf9.chunk.js"
+  },
+  {
+    "revision": "fbcb212c233f36c77380",
+    "url": "/react_native_challenge/static/js/runtime-main.bc0b54f0.js"
+  }
+]);
