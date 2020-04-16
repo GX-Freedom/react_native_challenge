71192f61f0e5f0eda1d390c7253055fec37b3816
GX-Freedom <kjy911206@gmail.com>
2020-04-16 14:55:29 +0900

    Updates

---
+++ precache-manifest.d38ab270118170c5b0dfea7190ffc170.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f05d6fc155d5bc1836f4ef023e32faa",
+    "url": "/react_native_challenge/index.html"
+  },
+  {
+    "revision": "d2dfe79ee6600209cbb8",
+    "url": "/react_native_challenge/static/js/2.d24a8ef3.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/react_native_challenge/static/js/2.d24a8ef3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3447e796525ddfc02a1",
+    "url": "/react_native_challenge/static/js/main.5d1af7fe.chunk.js"
+  },
+  {
+    "revision": "fbcb212c233f36c77380",
+    "url": "/react_native_challenge/static/js/runtime-main.bc0b54f0.js"
+  },
+  {
+    "revision": "82835079a3861d85f7aa9fea3ae32b71",
+    "url": "/react_native_challenge/static/media/time.82835079.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.619026b2b8dbd87e5770c6840a396dfe.js"
+  "/react_native_challenge/precache-manifest.d38ab270118170c5b0dfea7190ffc170.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_native_challenge/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
